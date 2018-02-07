8694e3aad85a1389b5377b047c2fe7b4eb3bf045
Jali Rainio <jali.rainio@gmail.com>
2018-02-07 11:55:29 +0200

    Album view works

--- djangonaut/djangonaut/settings.py
+++ djangonaut/djangonaut/settings.py
@@ -126,10 +126,3 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
-# Update database configuration with $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
